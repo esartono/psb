442d6898b3d9bf6cdc2337fe58fa80a402ef105e
esartono <eko.sartono@nurulfikri.sch.id>
2021-09-27 13:09:01 +0700

    perbaikan tampilan tulisan cetak wawancara keuangan

--- resources/views/user/tahapan/3.blade.php
+++ resources/views/user/tahapan/3.blade.php
@@ -2,4 +2,4 @@
 <p>Pengumuman hasil akan dilaksanakan secara online pada tanggal : <b>{{ ($calon->jadwal->seleksi) ? $calon->jadwal->pengumuman->isoFormat('D MMMM Y') : ""}}</b> </p>
 <hr>
 <a href='/seleksiPDF/{{ $calon->id }}' class="btn btn-success mt-3 col-md-8" target="_blank"><b>Cetak Kartu Peserta</b></a>
-<a href='/printTagihanPPDB/{{ $calon->id }}' class="btn btn-danger mt-3 col-md-8" target="_blank"><b>Cetak Form Wawancara Keuangan - PPDB SIT Nurul Fikri</b></a>
+<a href='/printTagihanPPDB/{{ $calon->id }}' class="btn btn-danger mt-3 col-md-8" target="_blank"><b>Cetak Form Wawancara Keuangan <br> PPDB SIT Nurul Fikri</b></a>
