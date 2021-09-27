2bca099a2e512120c3dcaa7bebc17be2b9fb9429
esartono <eko.sartono@nurulfikri.sch.id>
2021-09-27 13:07:26 +0700

    Ubah tulisan cetak tagihan di dashboard Ortu

--- resources/views/user/tahapan/3.blade.php
+++ resources/views/user/tahapan/3.blade.php
@@ -1,5 +1,5 @@
 <h4 class="timeline-header">Pengumuman Hasil Tes - Online </h4> <br>
 <p>Pengumuman hasil akan dilaksanakan secara online pada tanggal : <b>{{ ($calon->jadwal->seleksi) ? $calon->jadwal->pengumuman->isoFormat('D MMMM Y') : ""}}</b> </p>
 <hr>
-<a href='/seleksiPDF/{{ $calon->id }}' class="btn btn-success mt-3 col-md-6" target="_blank"><b>Cetak Kartu Peserta</b></a>
-<a href='/printTagihanPPDB/{{ $calon->id }}' class="btn btn-danger mt-3 col-md-6" target="_blank"><b>Cetak Tagihan PPDB - SIT Nurul Fikri</b></a>
+<a href='/seleksiPDF/{{ $calon->id }}' class="btn btn-success mt-3 col-md-8" target="_blank"><b>Cetak Kartu Peserta</b></a>
+<a href='/printTagihanPPDB/{{ $calon->id }}' class="btn btn-danger mt-3 col-md-8" target="_blank"><b>Cetak Form Wawancara Keuangan - PPDB SIT Nurul Fikri</b></a>
